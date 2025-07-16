f7c3dbbe7a5b6474fd5588b01961df52042e34d3
Jan Shareef Sayyad <janshareef.sd@gmail.com>
2025-07-16 10:18:46 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
   }
 });
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigins,credentials:true}));
+// app.use(cors({origin: allowedOrigins,credentials:true}));
 
 app.post("/stripe",express.raw({type:'application/json'}),stripeWebHooks)
 
