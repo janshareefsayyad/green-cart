e4df37d3614efc314b6098ae0103ab31b83e5120
janshareefsayyad <janshareef.sd@gmail.com>
2025-07-16 11:35:06 +0530

    f

--- server/server.js
+++ server/server.js
@@ -21,8 +21,16 @@ await connectCloudinary();
 //Allow multiple Origins
 const allowedOrigins = ['http://localhost:5173',process.env.CLIENT_ORIGIN];
 
-app.use(express.json());
-
+//Middleware Configuration
+app.use((req, res, next) => {
+  if (req.originalUrl === '/stripe') {
+    // Don't parse webhook requests as JSON
+    next();
+  } else {
+    // Parse all other requests as JSON
+    express.json()(req, res, next);
+  }
+});
 app.use(cookieParser());
 app.use(cors({origin: allowedOrigins,credentials:true}));
 
